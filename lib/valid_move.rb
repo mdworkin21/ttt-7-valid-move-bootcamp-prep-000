833a9115b8c71e35792921a54cede1c81a217ab5
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-27 14:52:01 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,9 +5,13 @@ def valid_move?(array, index)
   else
     return false
   end
+  if array[index] ==  (0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8)
+    return true
+  else
+    return false
+  end
 end
 
-# array[index] ==  (0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8)
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(array, index)
